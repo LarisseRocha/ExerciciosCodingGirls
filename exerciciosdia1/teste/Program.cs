62da76f0ae9b5e3f94bd4d68fcdc95bb9b9414ea
LarisseRocha <larisse16rocha@gmail.com>
2022-06-07 12:17:34 -0300

    exerciciodia1

---
+++ exerciciosdia1/teste/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Elabore um programa que escreve seu nome completo,
+            // seu endereço, o CEP e telefone em linhas separadas.
+            Console.WriteLine("Exercício 1: ");
+
+            Console.WriteLine("Escreva seu nome completo: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Escreva seu nome endereço: ");
+            string endereco = Console.ReadLine();
+
+            Console.WriteLine("Escreva seu CEP: ");
+            string cep = Console.ReadLine();
+
+            Console.WriteLine("Escreva seu número de telefone: ");
+            string tel = Console.ReadLine();
+
+            Console.WriteLine("\n");
+
+
+
+            //Escolha uma mulher famosa na história da tecnologia e implemente um programa que escreve seu nome,
+            //sua formação e uma contribuição feita por ela dentro da tecnologia em linhas separadas.
+            Console.WriteLine("Exercício 2");
+            Console.WriteLine("Escreva o nome de uma mulher famosa:");
+            string nomeFamosa = Console.ReadLine();
+
+            Console.WriteLine("Formação:");
+            string formacao = Console.ReadLine();
+
+            Console.WriteLine("Contribuição: ");
+            string contribuicao = Console.ReadLine();
+
+            Console.WriteLine("\n");
+
+            //Elabore um programa que mostre na tela uma letra de música que você gosta,
+            //o compositor e o gênero musical em linhas separadas.
+
+            Console.WriteLine("Exercício 3");
+            Console.WriteLine("Escreva a letra:");
+            string letra = Console.ReadLine();
+
+            Console.WriteLine("Compositor:");
+            string compositor = Console.ReadLine();
+
+            Console.WriteLine("Gênero: ");
+            string genero = Console.ReadLine();
+
+            //Elabore um programa que exibe uma mensagem que incentive outras mulheres
+            //a entrar na tecnologia.
+
+
+
+
+
+        }
+
+    }
+}
