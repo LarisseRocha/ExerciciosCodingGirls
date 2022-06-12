0cb11021915ac8799a618fcebde7d3f86696e415
LarisseRocha <larisse16rocha@gmail.com>
2022-06-11 23:39:07 -0300

    renomeei as pastas

---
+++ aula4/exercicio2/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace exercicios4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ex2();
+
+        }
+
+
+        static void Ex2()
+        {
+            int[] numeros = new int[10];
+
+            int[] vetPar = new int[10];
+
+            int[] vetImpar = new int[10];
+
+            int contPar = 0;
+
+            int contImpar = 0;
+
+            Console.WriteLine("Digite os números: ");
+
+            for (int i = 0; i < 10; i++)
+            {
+                numeros[i] = int.Parse(Console.ReadLine());
+
+                if (numeros[i] % 2 == 0)
+                {
+                    vetPar[contPar] = numeros[i];
+                    contPar++;
+
+                }
+
+                else
+                {
+                    vetImpar[contImpar] = numeros[i];
+                    contImpar++;
+
+                }
+            }
+
+            Console.WriteLine("Números pares:\n");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"{vetPar[i]}-");
+
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Números ímpares:\n");
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.Write($"{vetImpar[i]}-");
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ aula4/exercicio3/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace exercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Ex3();
+        }
+
+
+        static void Ex3()
+        {
+            string[] palavras = new string[5];
+
+            string[] vetAux = new string[5];
+
+            Console.WriteLine("Digite as palavras:\n");
+
+            for (int i = 0; i < 5; i++)
+            {
+                palavras[i] = Console.ReadLine();
+                ;
+                Console.WriteLine("\n");
+
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                Console.WriteLine(palavras[i].Length);
+                    }
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
+
