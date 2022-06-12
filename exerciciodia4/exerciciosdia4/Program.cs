9a21e7d6a657d2e4efa06947a975f7a9b6f2c9a8
LarisseRocha <larisse16rocha@gmail.com>
2022-06-11 23:02:02 -0300

    Exercício 3 da aula 4

---
+++ exerciciodia4/exerciciosdia4/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace exercicios4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ex1();
+
+        }
+
+
+        static void Ex1()
+        {
+
+            Console.WriteLine("Digite os 10 números:");
+            int[] numeros = new int[10];
+            int[] vetNum = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+
+                numeros[i] = int.Parse(Console.ReadLine());
+                vetNum[i] = numeros[i] * 5;
+
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("A multiplicação dos números é igual a:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(vetNum[i]);
+
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
+
